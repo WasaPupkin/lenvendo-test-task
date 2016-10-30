dae6c27d08e596d8eab3e4745098b4ad29a953ca
Lvov W <lvov.v@topsdigital.ru>
2016-10-30 14:43:24 +0300

    Init commit

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=',
+//    'dsn' => 'mysql:host=localhost;port=3306;dbname=',
+    'username' => 'dev',
+    'password' => '123456',
+    'charset' => 'utf8',
+    'schemaMap' => [
+        'pgsql'=> [
+            'class'=>'yii\db\pgsql\Schema',
+            'defaultSchema' => 'public' //specify your schema here
+        ]
+    ],
+    'enableSchemaCache' => true,
+
+    // Duration of schema cache.
+    'schemaCacheDuration' => 3600,
+
+    // Name of the cache component used to store schema information
+    'schemaCache' => 'cache',
+];
